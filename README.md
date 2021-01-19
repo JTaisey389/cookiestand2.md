a4b8d236812adc4b4fba6da0421f4db72cdeadf8
Jason Taisey <jdtaisey89@icloud.com>
2021-01-18 22:07:47 -0800

    Created functions for random Number

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # cookiestand2
 Seattle Cookie Stand 
+
+# cookiestand
+Cookie-Stand
+
+Monday January 18 
+Modeling Problem Domain
+
+# Problem Domain
+
+A place that is called Seattle Kitten Resue Service, tons of kittens that need homes. They need a website that can match perspective owners with rescue kitens. Having hundreds of kittens, they want a plave for users to find kittens and get info about those kittens and make desciisions based on information that the SKR has. 
+
+- As the pet owner, I would like a profile of kittens that SKR has rescued.
+  - Personality Traits
+  - Name
+  - Breed
+  - Image of the cat
+  - Health History 
+- As the rescue service 
+  - Who is adopting
+    - Name
+    - Age
+    - Pet history as an owner
+    - Address 
+
+Out of Scope
+- Removing kittens from a list
+- Social Media
+- No Videos
+
+## Specific Taks to Complete
+- Dispaly a list of cats that need adopting
+- Create a form for adding cats to our list
+- Create a form for perspective cat owners 
+
+## Idea for the page
---
+++ app.js
@@ -0,0 +1,50 @@
+'use strict';
+// Seattle 
+function getRanNum(a, b,) {
+  return Math.random() * (a - b);
+  ;
+}
+console.log(Math.random() * (65 - 23) + 23);
+
+// console.log(getRanNum(65, 23));
+
+// // Tokyo
+function getRanNum(a, b,) {
+  return Math.random() * (a - b);
+  ;
+}
+console.log(Math.random() * (24 - 3) + 3);
+
+// // Dubai 
+function getRanNum(a, b,) {
+  return Math.random() * (a - b);
+  ;
+}
+console.log(Math.random() * (38 - 11) + 11);
+
+// // Paris
+function getRanNum(a, b,) {
+  return Math.random() * (a - b);
+  ;
+}
+console.log(Math.random() * (38 - 20) + 20);
+
+// // Lima
+function getRanNum(a, b,) {
+  return Math.random() * (a - b);
+  ;
+}
+console.log(Math.random() * (16 - 2) + 2);
+
+
+
+// // These are the stepts to append in Javascript 
+// var divElement = document.createElement('div'); // this targets the div specifically 
+
+// var h3Elemment = document.createElement('h2');
+
+// h2Elment.textContent = dest1.name 
+
+// var sectionElement = documentElementByID('profiles');
+// sectionElement.appendChild(divElement);
+
