e9a87b366ddd9b355fabceaa731b2eee26e5c62e
Jason Taisey <jdtaisey89@icloud.com>
2021-01-19 22:39:31 -0800

    Added Hourly Lists

--- app.js
+++ app.js
@@ -1,50 +1,92 @@
 'use strict';
-// Seattle 
-function getRanNum(a, b,) {
-  return Math.random() * (a - b);
-  ;
+var elTarget = document.getElementById('table-body');
+var table = document.createElement('table');// BODY
+elTarget.appendChild(table);// Table Head Created
+var tableHead = document.createElement('thead');
+table.appendChild(tableHead);// Table Body
+var tableBody = document.createElement('tbody');
+table.appendChild(tableBody);
+
+var openHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', 'Totals'];
+
+var tableStart = document.createElement('th');
+tableStart.textContent = '';
+
+tableHead.appendChild(tableStart);
+
+for (var a = 0; a < openHours.length; a++) {
+  var tableStart = document.createElement('th');
+  tableStart.textContent = openHours[a];
+  tableHead.appendChild(tableStart);
 }
-console.log(Math.random() * (65 - 23) + 23);
-
-// console.log(getRanNum(65, 23));
-
-// // Tokyo
-function getRanNum(a, b,) {
-  return Math.random() * (a - b);
-  ;
+// Constructor Function for Table
+function Stores(store, minCust, maxCust, avgCookieSales) {
+  this.name = store;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgCookieSales = avgCookieSales;
+  this.totalcookiesPerday = 0
+  this.array = [];
 }
-console.log(Math.random() * (24 - 3) + 3);
 
-// // Dubai 
-function getRanNum(a, b,) {
-  return Math.random() * (a - b);
-  ;
+var openHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', 'Totals'];
+var storeLocations = [Seattle, Tokyo, Dubai, Paris, Lima];
+
+// Constructor Function for Rand Nums
+function Stores(store, minCust, maxCust, avgCookieSales) {
+  this.name = store;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgCookieSales = avgCookieSales;
+  this.totalcookiesPerday = 0
+  this.array = [];
 }
-console.log(Math.random() * (38 - 11) + 11);
-
-// // Paris
-function getRanNum(a, b,) {
-  return Math.random() * (a - b);
-  ;
+// Function Prototype
+Stores.prototype.numOfcustomers = function () {
+  // var totalcookiesPerday = 0;
+  for (var i = 0; i < openHours.length - 1; i++) {
+    var cookieshour = Math.floor((Math.random() * (this.maxCust - this.minCust + 1) + this.minCust) * this.avgCookieSales);
+    this.totalcookiesPerday += cookieshour;
+    this.array.push(cookieshour);
+  }
+  this.array.push(this.totalcookiesPerday);
 }
-console.log(Math.random() * (38 - 20) + 20);
 
-// // Lima
-function getRanNum(a, b,) {
-  return Math.random() * (a - b);
-  ;
+Stores.prototype.printTable = function () {
+  var row = document.createElement('tr');
+  var nameCell = document.createElement('td');
+  tableBody.appendChild(row);
+  nameCell.textContent = this.name;
+  row.append(nameCell);
+
+  for (var i = 0; i < openHours.length; i++) {
+    var dataCell = document.createElement('td');
+    dataCell.textContent = this.array[i];
+    row.appendChild(dataCell);
+  }
 }
-console.log(Math.random() * (16 - 2) + 2);
+// Declaring Functions and printing to the tables
+var Seattle = new Stores('Seattle', 23, 65, 6.3);
+Seattle.numOfcustomers();
+Seattle.printTable();
+var Tokyo = new Stores('Tokyo', 3, 24, 1.2);
+Tokyo.numOfcustomers();
+Tokyo.printTable();
+var Dubai = new Stores('Dubai', 11, 38, 3.7);
+Dubai.numOfcustomers();
+Dubai.printTable();
+var Paris = new Stores('Paris', 20, 38, 2.3);
+Paris.numOfcustomers();
+Paris.printTable();
+var Lima = new Stores('Lima', 2, 16, 4.6);
+Lima.numOfcustomers();
+Lima.printTable();
 
+console.log(Seattle.array);
+console.log(Tokyo.array);
+console.log(Dubai.array);
+console.log(Paris.array);
+console.log(Lima.array);
+var storeLocations = [Seattle, Tokyo, Dubai, Paris, Lima];
 
-
-// // These are the stepts to append in Javascript 
-// var divElement = document.createElement('div'); // this targets the div specifically 
-
-// var h3Elemment = document.createElement('h2');
-
-// h2Elment.textContent = dest1.name 
-
-// var sectionElement = documentElementByID('profiles');
-// sectionElement.appendChild(divElement);
 
---
+++ app2.js
@@ -0,0 +1,59 @@
+// // Table Creation
+// var tableBody = document.getElementById('table-body');
+
+// var = location = []
+// // Row 1 Creation
+// {var rowOne= document.createElement ('tr');
+// var cellOne = document.createElement ('td');
+
+// cellOne.textContent = '';
+// cellTwo.textContent = '6:00 am';
+
+
+// rowOne.appendChild(cellOne);
+// rowOne.appendChild(cellTwo);
+
+// // Tablebody.innerHTML ='';
+// var openHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+// var locations = ['Seattle', 'Tokyo', 'Dubai', 'Paris', 'Lima'];
+
+// function Store (name, DailyTot) {
+//   this.name = name;
+//   this.hours = hours;
+//   this.tot = DailyTot;
+// }
+// var Seattle = new Location ('Seattle', 'Daily Location Total')
+// var Tokyo = new Location ('Tokyo', 'Daily Location Total')
+// var Dubai = new Location ('Dubai', 'Daily Location Total')
+// var Paris = new Location ('Paris', 'Daily Location Total')
+// var Lima = new Location ('Lima', 'Daily Location Total')
+
+// function getRanNum(a, b,) {
+//   return Math.random() * (a - b);
+//   ;
+// }
+// console.log(Math.random() * (65 - 23) + 23);
+
+// function getRanNum(a, b,) {
+//   return Math.random() * (a - b);
+//   ;
+// }
+// console.log(Math.random() * (24 - 3) + 3);
+
+// function getRanNum(a, b,) {
+//   return Math.random() * (a - b);
+//   ;
+// }
+// console.log(Math.random() * (38 - 11) + 11);
+
+// function getRanNum(a, b,) {
+//   return Math.random() * (a - b);
+//   ;
+// }
+// console.log(Math.random() * (38 - 20) + 20);
+
+// function getRanNum(a, b,) {
+//   return Math.random() * (a - b);
+//   ;
+// }
+// console.log(Math.random() * (16 - 2) + 2);
